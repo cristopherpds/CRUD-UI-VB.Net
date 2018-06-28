8e3170bc9605a718524db5f9ffcf69c749c5869b
Cristopher Paiva <josuepaiva7@gmail.com>
2018-06-28 14:32:30 -0300

    lol
    mimi

--- UIproyecto/CapaEntidades/Conexion.vb
+++ UIproyecto/CapaEntidades/Conexion.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Conexion
 
-    Public Const cnn As String = " server=Localhost; DataBase=lul; Uid=root; Pwd="
+    Public Const cnn As String = " server=Localhost; DataBase=lul; Uid=root; Pwd=111"
 End Class
 
 
